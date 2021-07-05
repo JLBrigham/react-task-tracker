4c4d5d0e8bc36f6dbab8a636c611cd2974e0d006
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-07-05 11:40:53 +0200

    Default props and propTypes added to Button component

--- react-task-tracker/src/components/Button.js
+++ react-task-tracker/src/components/Button.js
@@ -1,7 +1,18 @@
+import PropTypes from 'prop-types'
+
 function Button(props) {
     return (
 <button className='btn' style={{ backgroundColor: props.color}}>{props.text}</button>
     )
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
 }
 
 export default Button
