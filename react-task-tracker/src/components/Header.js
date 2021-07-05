d63046ddc741a1439d356a495168657ff22b4602
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-07-05 11:13:00 +0200

    Added default props and prop types to Header component

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,9 +1,19 @@
-function Header() {
+import PropTypes from 'prop-types'
+
+function Header(props) {
     return (
         <header>
-<h1>Task Tracker</h1>
+<h1>{props.title}</h1>
         </header>
     )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
 }
 
 export default Header
